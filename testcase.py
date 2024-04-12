4f5818d7ca5fe94bdb7cb060fa9e3f0b5ac7bb08
i-sanjay-cs <sanjay826870@gmail.com>
2024-04-12 18:55:25 +0530

    Frst commit

---
+++ testcase.py
@@ -0,0 +1,18 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calculator.add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(self.calculator.subtract(5, 3), 2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calculator.multiply(2, 3), 6)
+
+if __name__ == '__main__':
+    unittest.main()
